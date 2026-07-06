48aadc39563d350b51b9105f81772580b3f6a95f
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "sentinel_data_connector_iots" {
   description = "All sentinel_data_connector_iot resources"
   value       = azurerm_sentinel_data_connector_iot.sentinel_data_connector_iots
 }
+output "sentinel_data_connector_iots_log_analytics_workspace_id" {
+  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_iots"
+  value       = [for k, v in azurerm_sentinel_data_connector_iot.sentinel_data_connector_iots : v.log_analytics_workspace_id]
+}
+output "sentinel_data_connector_iots_name" {
+  description = "List of name values across all sentinel_data_connector_iots"
+  value       = [for k, v in azurerm_sentinel_data_connector_iot.sentinel_data_connector_iots : v.name]
+}
+output "sentinel_data_connector_iots_subscription_id" {
+  description = "List of subscription_id values across all sentinel_data_connector_iots"
+  value       = [for k, v in azurerm_sentinel_data_connector_iot.sentinel_data_connector_iots : v.subscription_id]
+}
 
